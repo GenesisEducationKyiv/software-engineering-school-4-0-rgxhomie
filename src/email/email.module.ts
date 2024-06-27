ce9e382eb5cec20d6cadf7e7fda31c0e7027e5ac
Borys Hrimov <b.hrimov00@gmail.com>
2024-06-27 17:20:22 +0300

    used factory for rate and mailer providers

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { MailerService } from 'src/mailer/mailer.service';
-import { RateService } from 'src/rate/rate.service';
+import { SmtpMailerCreator } from 'src/mailer/smtp/smtpMailer.creator';
+import { NbuProviderCreator } from 'src/rate/nbu/nbuProvider.creator';
 
 @Module({
-  providers: [EmailService, PrismaService, MailerService, RateService],
+  providers: [EmailService, PrismaService, NbuProviderCreator, SmtpMailerCreator],
 })
 export class EmailModule {}
---
+++ src/mailer/types/mailer.interface.ts
@@ -0,0 +1,11 @@
+export interface ISendMailConfig {
+    from: string,
+    to: string,
+    subject: string,
+    text?: string,
+    html?: string
+}
+
+export interface IMailer {
+    send(config: ISendMailConfig): void
+}
